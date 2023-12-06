6aa0a706e16a6eb1673524211118e36c2916bca1
Katelyn Gigante <clockwork.singularity@gmail.com>
2023-12-06 11:25:44 +1100

    Add the ability to use traps as filler, and a yaml option to configure how much to do it

--- worlds/_manual/Options.py
+++ worlds/_manual/Options.py
@@ -3,9 +3,12 @@ from Options import FreeText, NumericOption, Toggle, DefaultOnToggle, Choice, Te
 from .hooks.Options import before_options_defined, after_options_defined
 
 
+class FillerTrapPercent(Range):
+    """How many fillers will be replaced with traps. 0 means no additional traps, 100 means all fillers are traps."""
+    range_end = 100
 
 manual_options = before_options_defined({})
 
-# Manual can do things to define options here, though it doesn't currently
+manual_options["filler_traps"] = FillerTrapPercent
 
 manual_options = after_options_defined(manual_options)

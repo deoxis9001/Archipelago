c32c5aced43362666f7e75eaca378babbadb2bf8
Dinopony <dinopony.gg@gmail.com>
2024-03-12 14:54:13 +0100

    OoS: Fixed receiving items during Blaino's fight

--- worlds/tloz_oos/Client.py
+++ worlds/tloz_oos/Client.py
@@ -9,6 +9,9 @@ from .Data import build_item_id_to_name_dict, build_location_name_to_id_dict
 if TYPE_CHECKING:
     from worlds._bizhawk.context import BizHawkClientContext
 
+ROOM_AFTER_DRAGONOX = 0x0790
+ROOM_BLAINOS_GYM = 0x03B4
+
 ROM_ADDRS = {
     "game_identifier": (0x0134, 9, "ROM"),
     "slot_name": (0xFFFC0, 64, "ROM"),
@@ -23,6 +26,7 @@ RAM_ADDRS = {
     "current_map_group": (0xCC49, 1, "System Bus"),
     "current_map_id": (0xCC4C, 1, "System Bus"),
 }
+
 
 class OracleOfSeasonsClient(BizHawkClient):
     game = "The Legend of Zelda - Oracle of Seasons"
@@ -88,16 +92,7 @@ class OracleOfSeasonsClient(BizHawkClient):
             num_received_items = int.from_bytes(read_result[1], "little")
             received_item_is_empty = (read_result[2][0] == 0)
             flag_bytes = read_result[3]
-            game_clear = (read_result[4][0] == 0x07 and read_result[5][0] == 0x90)
-
-            # If the game hasn't received all items yet and the received item struct doesn't contain an item, then
-            # fill it with the next item
-            if num_received_items < len(ctx.items_received) and received_item_is_empty:
-                next_item_name = self.item_id_to_name[ctx.items_received[num_received_items].item]
-                await bizhawk.write(ctx.bizhawk_ctx, [(0xCBFB, [
-                    ITEMS_DATA[next_item_name]["id"],
-                    ITEMS_DATA[next_item_name]["subid"] if "subid" in ITEMS_DATA[next_item_name] else 0
-                ], "System Bus")])
+            current_room = (read_result[4][0] << 8) | read_result[5][0]
 
             # Read location flags from RAM
             local_checked_locations = set(ctx.locations_checked)
@@ -147,7 +142,22 @@ class OracleOfSeasonsClient(BizHawkClient):
                     "create_as_hint": int(2)
                 }])
 
+            # Don't process received items if we are in Blaino's Gym to prevent him
+            # from calling us a cheater
+            if current_room == ROOM_BLAINOS_GYM:
+                return
+
+            # If the game hasn't received all items yet and the received item struct doesn't contain an item, then
+            # fill it with the next item
+            if num_received_items < len(ctx.items_received) and received_item_is_empty:
+                next_item_name = self.item_id_to_name[ctx.items_received[num_received_items].item]
+                await bizhawk.write(ctx.bizhawk_ctx, [(0xCBFB, [
+                    ITEMS_DATA[next_item_name]["id"],
+                    ITEMS_DATA[next_item_name]["subid"] if "subid" in ITEMS_DATA[next_item_name] else 0
+                ], "System Bus")])
+
             # Send game clear
+            game_clear = (current_room == ROOM_AFTER_DRAGONOX)
             if not ctx.finished_game and game_clear:
                 await ctx.send_msgs([{
                     "cmd": "StatusUpdate",
